e22a5b1f0aae17250034b35095953d23d4a5ccdb
MN-Pham <60745209+MN-Pham@users.noreply.github.com>
2020-06-03 11:08:08 +0200

    Fix the calculation

--- PT100.cpp
+++ PT100.cpp
@@ -8,21 +8,24 @@
 PT100::PT100 (ADS1015 &adc, const int channel, const int ID)
     : ADC_(adc), channel_(channel), Sensor(ID) {
     Wire.begin();
+    ADC_.setGain(ADS1015_CONFIG_PGA_1); // PGA gain set to 1, max=4.096V
 }
 
 /**
- * This function read value from the ADC.
+ * This function read value from the ADC and calculate the temperature.
  * @return NO_ERR 0 on success.
  * @return CONNECT_ERR 1 if the arduino cannot find the I2C slave.
- * @todo Calculate temperature from the resistance of PT100.
  */ 
 int PT100::Measure() {
     float voltage;
     if (!ADC_.isConnected()) {
         return CONNECT_ERR;
     }
-    voltage = ADC_.getSingleEnded(channel_);
-	Temperature_ = (voltage-1.98)/0.007623;
+    //signed 12 bit -> max count = 2047
+    //voltage = 4.096/2047 * result
+    voltage = (ADC_.getSingleEnded(channel_)*4.096)/2047.0;
+    Temperature_ = (voltage-1.98)/0.007623;
+    //Temperature_=voltage;
     return NO_ERR;
 }
 
